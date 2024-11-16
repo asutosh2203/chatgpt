8df4772b30366d55c26f49f294a1a68bc736b677
asutosh2203 <asutosh2001@gmail.com>
2024-11-16 21:23:20 +0530

    Removed query parameter from address bar

--- app/chat/[id]/page.tsx
+++ app/chat/[id]/page.tsx
@@ -1,8 +1,9 @@
-"use client";
+'use client';
 
-import Chat from "../../../components/Chat";
-import ChatInput from "../../../components/ChatInput";
-import { useSearchParams } from "next/navigation";
+import { useEffect } from 'react';
+import Chat from '../../../components/Chat';
+import ChatInput from '../../../components/ChatInput';
+import { useSearchParams } from 'next/navigation';
 type Props = {
   params: {
     id: string;
@@ -11,12 +12,23 @@ type Props = {
 
 const ChatPage = ({ params: { id } }: Props) => {
   const searchParams = useSearchParams();
-  const query = searchParams?.get("q");
-  
+  const query = searchParams?.get('q');
+
+  useEffect(() => {
+    // Get the current URL
+    const currentUrl = new URL(window.location as unknown as string);
+
+    // Remove the `q` parameter
+    currentUrl.searchParams.delete('q');
+
+    // Update the address bar without reloading the page
+    window.history.replaceState({}, '', currentUrl);
+  }, []);
+
   return (
-    <div className="flex flex-col h-screen overflow-hidden">
+    <div className='flex flex-col h-screen overflow-hidden'>
       <Chat chatId={id} />
-      <ChatInput chatId={id} query={query!}/>
+      <ChatInput chatId={id} query={query!} />
     </div>
   );
 };
